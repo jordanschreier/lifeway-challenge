6ae01f4ede9c7d9a71e78d92302ef674d13327c1
jordanschreier <redviper311@gmail.com>
2022-03-14 17:27:36 -0400

    Initial commit
    Provided by Spring Initializr

---
+++ src/main/java/com/jordanschreier/lifewaychallenge/LifewayChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.jordanschreier.lifewaychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifewayChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifewayChallengeApplication.class, args);
+	}
+
+}
